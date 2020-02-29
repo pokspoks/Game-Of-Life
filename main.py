43e7ddd7ad13c14afa639f6e2b048292f3115bc4
Poks <tehepicgames@gmail.com>
2020-02-29 16:56:25 +0200

    Add cmd args

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import boardGen
 import consoleRender
 import gameLogic
+import argparse
 from time import sleep
 
-width = 269
-height = 64
-board = boardGen.generate_board(width, height, True, 0.3)
+parser = argparse.ArgumentParser(description="Conway's game of life.")
+parser.add_argument('--width', '-W', nargs='?', default=30, type=int, help='Width of the game board as columns.')
+parser.add_argument('--height', '-H', nargs='?', default=30, type=int, help='Height of the game board as rows.')
+parser.add_argument('--speed', '-s', nargs='?', default=0.2, type=float, help='Game simulation speed.')
+parser.add_argument('--spawnrate', '-S', nargs='?', default=0.3, type=float, help='Spawn rate of cells.')
+
+args = parser.parse_args()
+
+width = args.width
+height = args.height
+board = boardGen.generate_board(width, height, True, args.spawnrate)
 
 while True:
     consoleRender.render_board(board)
     board = gameLogic.next_board_state(board)
-    sleep(0.1)
+    sleep(args.speed)
     consoleRender.clear_console()
