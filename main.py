6314f0db324c16ddec34313abbc7d400b9981edb
Poks <tehepicgames@gmail.com>
2020-03-02 12:47:38 +0200

    Change spawnrate arg to spawnchance

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description="Conway's game of life.")
 parser.add_argument('--width', '-W', nargs='?', type=int, default=30, help='Width of the game board in columns. Default=30')
 parser.add_argument('--height', '-H', nargs='?', type=int, default=30, help='Height of the game board in rows. Default=30')
 parser.add_argument('--speed', '-s', nargs='?', type=float, default=0.2, help='Game simulation speed in seconds. Default=0.2')
-parser.add_argument('--spawnrate', '-S', nargs='?', type=float, default=0.3, help='Spawn rate of cells as decimal percentage. Default=0.3')
+parser.add_argument('--spawnchance', '-c', nargs='?', type=float, default=0.3, help='Spawn chance of living cells as decimal percentage. Default=0.3')
 
 args = parser.parse_args()
 
