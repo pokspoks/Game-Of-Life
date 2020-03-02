bbe6fd2ceaca6936fb91cd2a3a5ef27e50ad9a7f
Poks <tehepicgames@gmail.com>
2020-03-02 19:38:14 +0200

    Change import order

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from time import sleep
+import argparse
+
 import boardGen
 import consoleRender
 import gameLogic
-import argparse
-from time import sleep
+
+
 
 parser = argparse.ArgumentParser(description="Conway's game of life.")
 parser.add_argument('--width', '-W', nargs='?', type=int, default=30, help='Width of the game board in columns. Default=30')
